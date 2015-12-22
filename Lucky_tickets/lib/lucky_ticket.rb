53638d47df63321c27e589bd3726ddb964b2872f
krlsdu <krlsdu@gmail.com>
2015-12-22 00:08:11 -0200

    Possible solution

--- Lucky_tickets/lib/lucky_ticket.rb
+++ Lucky_tickets/lib/lucky_ticket.rb
@@ -2,11 +2,44 @@ module LuckyTicket
   @number = '328940'
 
   def self.half
-    middle = (@number.count @number)/2
-    last  = @number.count(@number)
+    initial     = 0
+    length      = 4
+    max         = ('9' * length).to_i
+    sum         = 0
+    luck_ticket = 0
 
-    puts half_left = @number.slice(0...middle)
-    puts half_right  = @number.slice(middle..last)
+    (1..max).each { |value|
 
+      value = value.to_s
+
+      middle = (value.count value)/2
+      last   = value.count(value)
+
+      half_left  = value.slice(initial..middle)
+      half_right = value.slice(middle+1..last)
+
+      sum_l = sum_left(half_left)
+      sum_r = sum_right(half_right)
+
+      luck_ticket = luck_ticket + 1 if sum_l == sum_r
+
+
+    }
+
+    luck_ticket
+  end
+
+  def self.sum_left(half_left)
+    ary_left = []
+    ary_left = half_left.chars
+    ary_left.map!(&:to_i)
+    ary_left.reduce(:+)
+  end
+
+  def self.sum_right(half_right)
+    ary_right = []
+    ary_right = half_right.chars
+    ary_right.map!(&:to_i)
+    ary_right.reduce(:+)
   end
 end
