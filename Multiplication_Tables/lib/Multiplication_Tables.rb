8088ae0eecc1528425d9aee7c1a96e3b75703e33
krlsdu <krlsdu@gmail.com>
2016-08-03 20:32:28 -0300

    Improve Multiplication_Tables challenge

--- Multiplication_Tables/lib/Multiplication_Tables.rb
+++ Multiplication_Tables/lib/Multiplication_Tables.rb
@@ -1,13 +1,17 @@
-require "Multiplication_Tables/version"
-
-module MultiplicationTables
+#!/usr/bin/env ruby
+# require "Multiplication_Tables/version"
+class MultiplicationTables
   ARRAY1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
   ARRAY2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
-  def self.matrix
+  def matrix
     ARRAY1.each{ |num|
       ARRAY2.map {|multpli| print "%-4s" % [num * multpli]}
       puts ""
     }
   end
+end
+if __FILE__ == $0
+  Mult = MultiplicationTables.new
+  Mult.matrix
 end
