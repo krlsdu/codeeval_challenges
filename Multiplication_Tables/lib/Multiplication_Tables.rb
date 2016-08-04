d2ee72ce9afc09ceed47258459d655693bb74806
krlsdu <krlsdu@gmail.com>
2016-08-04 10:21:22 -0300

    Solution for the challenge

--- Multiplication_Tables/lib/Multiplication_Tables.rb
+++ Multiplication_Tables/lib/Multiplication_Tables.rb
@@ -1,20 +1,17 @@
 #!/usr/bin/env ruby
 
 module Multipli
-  Multipliclass Tables
-  MultipliclassARRAY1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-  11ARRAY2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-
-  11def self.matrix
-  11defARRAY1.each { |num|
-    matrixARRAY2.map { |multpli| print "#{num * multpli}".rjust(4) }
-
-    printputs ""
-    multpli}
-
-  printputsend
-  printputsendend
+	class Tables
+		ARRAY1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+		ARRAY2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+		def self.matrix
+			ARRAY1.each { |num|
+				ARRAY2.map { |multpli| print "#{num * multpli}".rjust(4) }
+				puts ""
+			}
+		end
+	end
 end
 if __FILE__ == $0
-  __FILE__Multipli::Tables.matrix
+	Multipli::Tables.matrix
 end
