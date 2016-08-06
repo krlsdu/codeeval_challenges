d5934fe35d2cc99286779a70bfbf57937453b8b1
krlsdu <krlsdu@gmail.com>
2016-08-06 14:07:26 -0300

    Result othe challenger

--- Sum_of_Primes/lib/Sum_of_Primes.rb
+++ Sum_of_Primes/lib/Sum_of_Primes.rb
@@ -1,5 +1,19 @@
-require "Sum_of_Primes/version"
-
+require "prime"
 module SumOfPrimes
-  # Your code goes here...
+	def self.total
+		sum = 0
+		array_prime = []
+		Prime.each(8000) do |prime|
+			array_prime.push(prime) if array_prime.size < 1000
+		end
+		array_prime.each do |number|
+			sum = number + sum
+		end
+		puts sum
+	end
 end
+
+if __FILE__ == $0
+	SumOfPrimes.total
+end
+
