ce1512c4641012b6a123766ce16689541a84725b
krlsdu <krlsdu@gmail.com>
2016-08-09 09:15:57 -0300

    Improve the code

--- Sum_of_Primes/lib/Sum_of_Primes.rb
+++ Sum_of_Primes/lib/Sum_of_Primes.rb
@@ -1,5 +1,24 @@
-require "Sum_of_Primes/version"
-
+require "prime"
 module SumOfPrimes
-  # Your code goes here...
+  ARRAY_PRIME = []
+  def self.total
+    find_thousand_prime
+
+    sum = 0
+    ARRAY_PRIME.each do |number|
+      sum = number + sum
+    end
+    puts sum
+  end
+
+  private
+  def self.find_thousand_prime
+    Prime.each(8000) do |prime|
+      ARRAY_PRIME.push(prime) if ARRAY_PRIME.size < 1000
+    end
+  end
+end
+
+if __FILE__ == $0
+  SumOfPrimes.total
 end
