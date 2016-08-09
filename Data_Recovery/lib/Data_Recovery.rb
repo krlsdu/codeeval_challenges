ebf2921831bdae0c26e3d67288597efb44880fb3
krlsdu <krlsdu@gmail.com>
2016-08-09 14:29:34 -0300

    building the solution

--- Data_Recovery/lib/Data_Recovery.rb
+++ Data_Recovery/lib/Data_Recovery.rb
@@ -1,5 +1,22 @@
-require "Data_Recovery/version"
+# require "Data_Recovery/version"
 
 module DataRecovery
-  # Your code goes here...
+  def self.split_words_hints(sentence_shuffled)
+    sentence_shuffled = sentence_shuffled.split(';')
+    words = sentence_shuffled[0].scan(/\w+/)
+    hints = sentence_shuffled[1].scan(/\w+/)
+
+    all_hints = find_number_missing(hints)
+    print all_hints
+  end
+  def self.find_number_missing(hints)
+   hints = hints.map(&:to_i)
+   complete = (hints.min..hints.max).to_a
+   hints|complete
+  end
+end
+
+File.open(ARGV[0]).each_line do |line|
+  DataRecovery.split_words_hints line
+
 end
